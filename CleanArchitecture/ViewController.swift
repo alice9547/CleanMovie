5a2aa34fc376ad19c44c323716de003331c02c43
김은서 <kes@quantec.co.kr>
2025-09-25 15:00:01 +0900

    Initial Commit

---
+++ CleanArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by 김은서 on 9/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CleanArchitectureTests/CleanArchitectureTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureTests.swift
+//  CleanArchitectureTests
+//
+//  Created by 김은서 on 9/25/25.
+//
+
+import Testing
+@testable import CleanArchitecture
+
+struct CleanArchitectureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
